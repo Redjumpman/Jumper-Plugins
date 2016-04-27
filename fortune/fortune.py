4fbf2c638cca911b767c36b219e36295251dc8c4
Redjumpman <chadwick.eh@gmail.com>
2016-04-27 09:50:29 -0400

    Update fortune.py
    cleaned up the code a bit

--- fortune/fortune.py
+++ fortune/fortune.py
@@ -1,17 +1,18 @@
-import discord
-from discord.ext import commands
-from random import randint
+ifrom discord.ext import commands
 from random import choice as randchoice
-import random
+
 
 class Fortune:
     """Fortune Cookie Commands."""
 
     def __init__(self, bot):
         self.bot = bot
-        self.fortune = ["He who laughs at himself never runs out of things to laugh at.","Man who fart in church sit in his own pew",
-                        "Man who go to bed with itchy butt wake up with stinky finger", "There is no I in team but U in cunt",
-                        "Gay man always order same, Sum Yung Guy", "Man piss in wind, wind piss back"]
+        self.fortune = ["He who laughs at himself never runs out of things to laugh at.",
+                        "Man who fart in church sit in his own pew",
+                        "Man who go to bed with itchy butt wake up with stinky finger",
+                        "There is no I in team but U in cunt",
+                        "Gay man always order same, Sum Yung Guy",
+                        "Man piss in wind, wind piss back"]
 
     @commands.command(name="fortune", aliases=["cookie"])
     async def _cookie(self):
@@ -20,6 +21,7 @@ class Fortune:
         And look deeply into my scales
         """
         return await self.bot.say("`" + randchoice(self.fortune) + "`")
+
 
 def setup(bot):
     bot.add_cog(Fortune(bot))
