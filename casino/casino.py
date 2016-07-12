ce38cfabf10d6a54a3f791b17f490fcc44302350
Redjumpman <chadwick.eh@gmail.com>
2016-07-12 12:17:50 -0400

    Update casino.py
    hotfix for casino info command

--- casino/casino.py
+++ casino/casino.py
@@ -25,7 +25,7 @@ class Casino:
     def __init__(self, bot):
         self.bot = bot
         self.casinosys = fileIO("data/casino/casino.json", "load")
-        self.games = ["Cups", "Coin", "Dice", "Blackjack", "Allin"]
+        self.games = ["Allin", "Blackjack", "Coin", "Cups", "Dice"]
         self.deck = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
         self.card_values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
                             '10': 10, 'Jack': 10, 'Queen': 10, 'King': 10}
