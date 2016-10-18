0bdf9b4732f6d31748b78abc397a20e6c11d80ab
Redjumpman <chadwick.eh@gmail.com>
2016-10-18 19:03:35 -0400

    Update animelist.py
    test for webhook

--- animelist/animelist.py
+++ animelist/animelist.py
@@ -1,6 +1,6 @@
 # Developed by Redjumpman for Redbot by Twentysix26
-# Ported from mee6bot to work for Red
-# Original credit and design goes to mee6
+# Ported from mee6bot to work for Red.
+# Original credit and design goes to mee6.
 import aiohttp
 import html
 import os
