79db507a7d27a9aa93ca49f1f23364a3392e48ec
Redjumpman <Redjumpman@users.noreply.github.com>
2016-11-18 07:40:49 -0500

    Update animelist.py
    Fixed an issue when requesting anime and manga information
    Converted from fileIO to dataIO
    Condensed code

--- animelist/animelist.py
+++ animelist/animelist.py
@@ -2,15 +2,28 @@
 # Ported from mee6bot to work for Red
 # Original credit and design goes to mee6
 import aiohttp
-import html
 import os
-from xml.etree import ElementTree
+import html
+from xml.etree import ElementTree as ET
 from discord.ext import commands
-from .utils.dataIO import fileIO
+from .utils.dataIO import dataIO
 from cogs.utils import checks
 
-# Username and Password from myanime list webiste
+# Username and Password is obtained from myanime list website
 # You need to create an account there and input the information below
+
+switcher = [
+    'english',
+    'score',
+    'type',
+    'episodes',
+    'volumes',
+    'chapters',
+    'status',
+    'start_date',
+    'end_date',
+    'synopsis'
+    ]
 
 
 class Animelist:
@@ -18,153 +31,87 @@ class Animelist:
 
     def __init__(self, bot):
         self.bot = bot
-        self.credentials = fileIO("data/animelist/credentials.json", "load")
+        self.file_path = "data/animelist/credentials.json"
+        self.credentials = dataIO.load_json(self.file_path)
 
     @commands.command(pass_context=True)
     @checks.is_owner()
     async def animeset(self, ctx):
         """Sets your username and password from myanimelist"""
         await self.bot.whisper("Type your user name. You can reply in this private msg")
-        username = await self.bot.wait_for_message(timeout=15,
-                                                   author=ctx.message.author)
+        username = await self.bot.wait_for_message(timeout=15, author=ctx.message.author)
         if username is None:
             return
         else:
             self.credentials["Username"] = username.content
-            fileIO("data/animelist/credentials.json", "save", self.credentials)
+            dataIO.save_json(self.file_path, self.credentials)
             await self.bot.whisper("Ok thanks. Now what is your password?")
-            password = await self.bot.wait_for_message(timeout=15,
-                                                       author=ctx.message.author)
+            password = await self.bot.wait_for_message(timeout=15, author=ctx.message.author)
             if password is None:
                 return
             else:
                 self.credentials["Password"] = password.content
-                fileIO("data/animelist/credentials.json", "save", self.credentials)
+                dataIO.save_json(self.file_path, self.credentials)
                 await self.bot.whisper("Setup complete. Account details added. Try searching for an anime using !anime")
 
     @commands.command(pass_context=True, no_pm=True)
-    async def anime(self, ctx, *, name):
+    async def anime(self, ctx, *, title):
         """Fetches info about an anime title!"""
-        username = self.credentials["Username"]
-        password = self.credentials["Password"]
-        anime = name.replace(" ", "_")
-        params = {
-            'q': anime
-                }
-        try:
-            auth = aiohttp.BasicAuth(login=username, password=password)
-            url = 'http://myanimelist.net/api/anime/search.xml?q=' + anime
-            with aiohttp.ClientSession(auth=auth) as session:
-                async with session.get(url, params=params) as response:
-                    data = await response.text()
-                    if data == '':
-                        await self.bot.say('I didn\'t found anything :cry: ...')
-                        return
-                    root = ElementTree.fromstring(data)
-                    if len(root) == 0:
-                        await self.bot.say('Sorry, I found nothing :cry:.')
-                    elif len(root) == 1:
-                        entry = root[0]
-                    else:
-                        msg = "**Please choose one by giving its number.**\n"
-                        msg += "\n".join(['{} - {}'.format(n+1, entry[1].text) for n, entry in enumerate(root) if n < 10])
-
-                        await self.bot.say(msg)  # Change to await response
-
-                        check = lambda m: m.content in map(str, range(1, len(root)+1))
-                        resp = await self.bot.wait_for_message(timeout=15, check=check)
-                        if resp is None:
-                            return
-
-                        entry = root[int(resp.content)-1]
-
-                    switcher = [
-                        'english',
-                        'score',
-                        'type',
-                        'episodes',
-                        'volumes',
-                        'chapters',
-                        'status',
-                        'start_date',
-                        'end_date',
-                        'synopsis'
-                        ]
-
-                    msg = '\n**{}**\n\n'.format(entry.find('title').text)
-                    for k in switcher:
-                        spec = entry.find(k)
-                        if spec is not None and spec.text is not None:
-                            msg += '**{}** {}\n'.format(k.capitalize()+':', html.unescape(spec.text.replace('<br />', '')))
-                    msg += 'http://myanimelist.net/anime/{}'.format(entry.find('id').text)
-
-                    await self.bot.say(msg)
-        except:
-            await self.bot.say("Your username or password is not correct." + "\n" +
-                               "You need to create an account on myanimelist.net ." +
-                               "\n" + "If you have an account use **<p>animeset** to set your credentials")
+        cmd = "anime"
+        await self.fetch_info(ctx, cmd, title)
 
     @commands.command(pass_context=True, no_pm=True)
-    async def manga(self, ctx, *, name):
+    async def manga(self, ctx, *, title):
         """Fetches info about an manga title!"""
+        cmd = "manga"
+        await self.fetch_info(ctx, cmd, title)
+
+    async def fetch_info(self, ctx, cmd, title):
+        data = await self.get_xml(cmd, title)
+
+        if data == '':
+            await self.bot.say("I couldn't find anything!")
+            return
+        try:
+            root = ET.fromstring(data)
+            if len(root) == 0:
+                await self.bot.say("I couldn't find anything!")
+            elif len(root) == 1:
+                entry = root[0]
+            else:
+                msg = "**Please choose one by giving its number.**\n"
+                msg += "\n".join(['{} - {}'.format(n+1, entry[1].text) for n, entry in enumerate(root) if n < 10])
+
+                await self.bot.say(msg)
+
+                check = lambda m: m.content in map(str, range(1, len(root)+1))
+                resp = await self.bot.wait_for_message(timeout=15, author=ctx.message.author, check=check)
+                if resp is None:
+                    return
+
+                entry = root[int(resp.content)-1]
+
+            msg = '\n**{}**\n\n'.format(entry.find('title').text)
+            for k in switcher:
+                spec = entry.find(k)
+                if spec is not None and spec.text is not None:
+                    msg += '**{}** {}\n'.format(k.capitalize()+':', html.unescape(spec.text.replace('<br />', '')))
+            msg += 'http://myanimelist.net/{}/{}'.format("anime", entry.find('id').text)
+
+            await self.bot.say(msg)
+        except:
+            await self.bot.say("Your username or password is not correct.You need to create an account on myanimelist.net.\nIf you have an account use **{}animeset** to set your credentials".format(ctx.prefix))
+
+    async def get_xml(self, nature, name):
         username = self.credentials["Username"]
         password = self.credentials["Password"]
-        manga = name.replace(" ", "_")
-        params = {
-            'q': manga
-                }
-        try:
-            auth = aiohttp.BasicAuth(login=username, password=password)
-            url = 'http://myanimelist.net/api/manga/search.xml?q=' + manga
-            with aiohttp.ClientSession(auth=auth) as session:
-                async with session.get(url, params=params) as response:
-                    data = await response.text()
-                    if data == '':
-                        await self.bot.say('I didn\'t found anything :cry: ...')
-                        return
-                    root = ElementTree.fromstring(data)
-                    if len(root) == 0:
-                        await self.bot.say('Sorry, I found nothing :cry:.')
-                    elif len(root) == 1:
-                        entry = root[0]
-                    else:
-                        msg = "**Please choose one by giving its number.**\n"
-                        msg += "\n".join(['{} - {}'.format(n+1, entry[1].text) for n, entry in enumerate(root) if n < 10])
-
-                        await self.bot.say(msg)  # Change to await response
-
-                        check = lambda m: m.content in map(str, range(1, len(root)+1))
-                        resp = await self.bot.wait_for_message(timeout=15, check=check)
-                        if resp is None:
-                            return
-
-                        entry = root[int(resp.content)-1]
-
-                    switcher = [
-                        'english',
-                        'score',
-                        'type',
-                        'episodes',
-                        'volumes',
-                        'chapters',
-                        'status',
-                        'start_date',
-                        'end_date',
-                        'synopsis'
-                        ]
-
-                    msg = '\n**{}**\n\n'.format(entry.find('title').text)
-                    for k in switcher:
-                        spec = entry.find(k)
-                        if spec is not None and spec.text is not None:
-                            msg += '**{}** {}\n'.format(k.capitalize()+':', html.unescape(spec.text.replace('<br />', '')))
-                    msg += 'http://myanimelist.net/manga/{}'.format(entry.find('id').text)
-
-                    await self.bot.say(msg)
-        except:
-            await self.bot.say("Your username or password is not correct." + "\n" +
-                               "You need to create an account on myanimelist.net ." +
-                               "\n" + "If you have an account use **<p>animeset** to set your credentials")
+        name = name.replace(" ", "_")
+        auth = aiohttp.BasicAuth(login=username, password=password)
+        url = 'https://myanimelist.net/api/{}/search.xml?q={}'.format(nature, name)
+        with aiohttp.ClientSession(auth=auth) as session:
+            async with session.get(url) as response:
+                data = await response.text()
+                return data
 
 
 def check_folders():
@@ -178,18 +125,9 @@ def check_files():
               "Password": ""}
 
     f = "data/animelist/credentials.json"
-    if not fileIO(f, "check"):
+    if not dataIO.is_valid_json(f):
         print("Adding animelist credentials.json...")
-        fileIO(f, "save", system)
-    else:  # consistency check
-        current = fileIO(f, "load")
-        if current.keys() != system.keys():
-            for key in system.keys():
-                if key not in current.keys():
-                    current[key] = system[key]
-                    print("Adding " + str(key) +
-                          " field to animelist credentials.json")
-            fileIO(f, "save", current)
+        dataIO.save_json(f, system)
 
 
 def setup(bot):
