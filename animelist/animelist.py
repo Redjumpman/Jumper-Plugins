d4775682bed53b4076584921d70a737dd9c606cd
Redjumpman <Redjumpman@users.noreply.github.com>
2016-12-12 17:34:54 -0500

    Animelist Update
    - [ENHANCEMENT] Set the output to an embed. Can click on the link to go to the animelist page.
    - [ENHANCEMENT] Added type to tell you if it's a TV show or Movie for anime.
    - [BUG] Fixed the image link not displaying properly
    - [CORE] PEP8 Compliance
    - [CORE] Enforced line width 100 characters

--- animelist/animelist.py
+++ animelist/animelist.py
@@ -1,9 +1,10 @@
-# Developed by Redjumpman for Redbot by Twentysix26.
+# Developed by Redjumpman for Twentysix26's Redbot.
 # Ported from mee6bot to work for Red
 # Original credit and design goes to mee6
 import aiohttp
 import os
 import html
+import discord
 from xml.etree import ElementTree as ET
 from discord.ext import commands
 from .utils.dataIO import dataIO
@@ -20,9 +21,9 @@ switcher = [
     'volumes',
     'chapters',
     'status',
+    'type',
     'start_date',
-    'end_date',
-    'synopsis'
+    'end_date'
     ]
 
 
@@ -52,7 +53,8 @@ class Animelist:
             else:
                 self.credentials["Password"] = password.content
                 dataIO.save_json(self.file_path, self.credentials)
-                await self.bot.whisper("Setup complete. Account details added. Try searching for an anime using !anime")
+                await self.bot.whisper("Setup complete. Account details added.\nTry searching for "
+                                       "an anime using {}anime".format(ctx.prefix))
 
     @commands.command(pass_context=True, no_pm=True)
     async def anime(self, ctx, *, title):
@@ -80,27 +82,38 @@ class Animelist:
                 entry = root[0]
             else:
                 msg = "**Please choose one by giving its number.**\n"
-                msg += "\n".join(['{} - {}'.format(n+1, entry[1].text) for n, entry in enumerate(root) if n < 10])
+                msg += "\n".join(['{} - {}'.format(n+1, entry[1].text)
+                                  for n, entry in enumerate(root) if n < 10
+                                  ])
 
                 await self.bot.say(msg)
 
                 check = lambda m: m.content in map(str, range(1, len(root)+1))
-                resp = await self.bot.wait_for_message(timeout=15, author=ctx.message.author, check=check)
+                resp = await self.bot.wait_for_message(timeout=15, author=ctx.message.author,
+                                                       check=check)
                 if resp is None:
                     return
 
                 entry = root[int(resp.content)-1]
 
-            msg = '\n**{}**\n\n'.format(entry.find('title').text)
+            link = 'http://myanimelist.net/{}/{}'.format("anime", entry.find('id').text)
+            embed = discord.Embed(colour=0x0066FF,
+                                  description=("All the information you need about "
+                                               "[{}]({})".format(entry.find('title').text, link)))
+            embed.title = entry.find('title').text
+            embed.set_thumbnail(url=entry.find('image').text)
+            embed.set_footer(text=entry.find('synopsis').text)
             for k in switcher:
                 spec = entry.find(k)
                 if spec is not None and spec.text is not None:
-                    msg += '**{}** {}\n'.format(k.capitalize()+':', html.unescape(spec.text.replace('<br />', '')))
-            msg += 'http://myanimelist.net/{}/{}'.format("anime", entry.find('id').text)
+                    embed.add_field(name=k.capitalize(),
+                                    value=html.unescape(spec.text.replace('<br />', '')))
 
-            await self.bot.say(msg)
+            await self.bot.say(embed=embed)
         except:
-            await self.bot.say("Your username or password is not correct.You need to create an account on myanimelist.net.\nIf you have an account use **{}animeset** to set your credentials".format(ctx.prefix))
+            await self.bot.say("Your username or password is not correct. You need to create an "
+                               "account on myanimelist.net.\nIf you have an account use "
+                               "**{}animeset** to set your credentials".format(ctx.prefix))
 
     async def get_xml(self, nature, name):
         username = self.credentials["Username"]
