4c24bf5d370faafe85ad6e83a6da35e3fc8d918f
Redjumpman <Redjumpman@users.noreply.github.com>
2017-02-07 01:29:39 -0600

    Patch 1.5.7
    Better balancing for Hi-Lo (Credit Marsh)

--- casino/casino.py
+++ casino/casino.py
@@ -32,7 +32,7 @@ except ImportError:
 server_default = {"System Config": {"Casino Name": "Redjumpman", "Casino Open": True,
                                     "Chip Name": "Jump", "Chip Rate": 1, "Default Payday": 100,
                                     "Payday Timer": 1200, "Threshold Switch": False,
-                                    "Threshold": 10000, "Credit Rate": 1, "Version": 1.56
+                                    "Threshold": 10000, "Credit Rate": 1, "Version": 1.57
                                     },
                   "Memberships": {},
                   "Players": {},
@@ -105,7 +105,7 @@ class CasinoBank:
     def __init__(self, bot, file_path):
         self.memberships = dataIO.load_json(file_path)
         self.bot = bot
-        self.patch = 1.56
+        self.patch = 1.57
 
     def create_account(self, user):
         server = user.server
@@ -332,7 +332,7 @@ class Casino:
         self.file_path = "data/JumperCogs/casino/casino.json"
         self.casino_bank = CasinoBank(bot, self.file_path)
         self.games = ["Blackjack", "Coin", "Allin", "Cups", "Dice", "Hi-Lo", "War"]
-        self.version = "1.5.6"
+        self.version = "1.5.7"
         self.cycle_task = bot.loop.create_task(self.membership_updater())
 
     @commands.group(pass_context=True, no_pm=True)
@@ -646,7 +646,7 @@ class Casino:
 
                 # Check for a 7 to give a 6x multiplier
                 if outcome[2] == "Seven":
-                    amount = bet * 6 #NOTE(Marsh): This is going to need balancing. It could be a good idea to make it a variable.  
+                    amount = bet * 6  
                     msg += "\n**BONUS!** 6x multiplier for Seven!"
                 else:
                     amount = int(round(bet * settings["Games"]["Hi-Lo"]["Multiplier"]))
