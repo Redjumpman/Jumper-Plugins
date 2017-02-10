488aab319762f894e058db2b9b86f1a4275faff6
OldManMarsh <fredrik_carlberg@hotmail.com>
2017-02-10 09:09:15 +0100

    Update casino.py

--- casino/casino.py
+++ casino/casino.py
@@ -32,7 +32,7 @@ except ImportError:
 server_default = {"System Config": {"Casino Name": "Redjumpman", "Casino Open": True,
                                     "Chip Name": "Jump", "Chip Rate": 1, "Default Payday": 100,
                                     "Payday Timer": 1200, "Threshold Switch": False,
-                                    "Threshold": 10000, "Credit Rate": 1, "Version": 1.571
+                                    "Threshold": 10000, "Credit Rate": 1, "Version": 1.581
                                     },
                   "Memberships": {},
                   "Players": {},
@@ -105,7 +105,7 @@ class CasinoBank:
     def __init__(self, bot, file_path):
         self.memberships = dataIO.load_json(file_path)
         self.bot = bot
-        self.patch = 1.571
+        self.patch = 1.581
 
     def create_account(self, user):
         server = user.server
@@ -332,7 +332,7 @@ class Casino:
         self.file_path = "data/JumperCogs/casino/casino.json"
         self.casino_bank = CasinoBank(bot, self.file_path)
         self.games = ["Blackjack", "Coin", "Allin", "Cups", "Dice", "Hi-Lo", "War"]
-        self.version = "1.5.7.1"
+        self.version = "1.5.8.1"
         self.cycle_task = bot.loop.create_task(self.membership_updater())
 
     @commands.group(pass_context=True, no_pm=True)
