e815bef608fb2d01ece7aa48700246a6232be752
Redjumpman <Redjumpman@users.noreply.github.com>
2017-03-14 14:42:22 -0500

    Dicetable 2.0
    New system will roll in the format of 2d12. 
    Uses the command dtable roll 2d20 2 0
    Uses modifier and defaults to rolling the die 1 time.
    Cleaned and condensed the code.

--- dicetable/dicetable.py
+++ dicetable/dicetable.py
@@ -1,225 +1,98 @@
+# DiceTable was created by Redjumpman for Red Bot
+
+# Standard Library
+import random
+import re
+
+# Discord
 from discord.ext import commands
 from __main__ import send_cmd_help
-import random
-try:   # Check if Tabulate is installed
+
+# Third Party Libraries
+try:
     from tabulate import tabulate
     tabulateAvailable = True
 except:
     tabulateAvailable = False
 
 
-class Dicetable:
+class FormatError(Exception):
+    pass
+
+
+class NegativeValue(Exception):
+    pass
+
+
+class RollError(Exception):
+    pass
+
+
+class DiceTable:
     """Rolls a table of dice"""
 
     def __init__(self, bot):
         self.bot = bot
+        self.group = []
+        self.version = 2.0
 
     @commands.group(pass_context=True)
-    async def dicetable(self, ctx):
+    async def dtable(self, ctx):
         """Shows a list under this group commands."""
 
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
 
-    @dicetable.command(name="d20", pass_context=False)
-    async def _d20_dicetable(self, number: int, modifier: int):
-        """Rolls a number of d20's plus your modifier
-        Example: 'dicetable d20 6 2' rolls 6 d20s with a +2 modifier"""
-        if number < 20:
-            roll_name = ["Roll 1", "Roll 2", "Roll 3", "Roll 4", "Roll 5",
-                         "Roll 6", "Roll 7", "Roll 8", "Roll 9", "Roll 10",
-                         "Roll 11",  "Roll 12",  "Roll 13",  "Roll 14",
-                         "Roll 15", "Roll 16",  "Roll 17",  "Roll 18",
-                         "Roll 19",  "Roll 20"
-                         ]
-            rolls = []
-            modplus = []
-            totals = []
+    @dtable.command(name="version")
+    async def _version_dtable(self):
+        await self.bot.say("You are running dicetable version {}".format(self.version))
 
-            k = "+" + str(modifier)
-            while number > 0:
-                b = random.randint(1, 20)
-                total = b + modifier
-                totals.append(total)
-                rolls.append(b)
-                modplus.append(k)
-                number = number-1
-            m = list(zip(roll_name, rolls,  modplus, totals))
-            t = tabulate(m, headers=["Roll #", "Results", "Modifier",
-                                     "Totals"
-                                     ]
-                         )
-            await self.bot.say("```" + str(t) + "```")
-        else:
-            await self.bot.say("```" + "Can only roll 20 dice at a time" +
-                               "```")
+    @dtable.command(name="roll", pass_context=False)
+    async def _roll_dtable(self, dice: str, times=1, modifier=0):
+        """Rolls a set of die in the format 2d12
+        By default only one set of die is rolled, without a
+        modifier. Example 3d20 5 1 will roll three 1d20 dice for
+        five seperate instances, with a +1 modifier added to the total."""
 
-    @dicetable.command(name="d12", pass_context=False)
-    async def _d12_dicetable(self, number: int, modifier: int):
-        """Rolls a number of d12's plus your modifier
-        Example: `dicetable d12 6 2` rolls 6 d12s with a +2 modifier"""
-        if number < 20:
-            roll_name = ["Roll 1", "Roll 2", "Roll 3", "Roll 4", "Roll 5",
-                         "Roll 6", "Roll 7", "Roll 8", "Roll 9", "Roll 10",
-                         "Roll 11",  "Roll 12",  "Roll 13",  "Roll 14",
-                         "Roll 15", "Roll 16",  "Roll 17",  "Roll 18",
-                         "Roll 19",  "Roll 20"
-                         ]
-            rolls = []
-            modplus = []
-            totals = []
+        if modifier < 0:
+            raise NegativeValue("Modifier can't be less than 0.")
 
-            k = "+" + str(modifier)
-            while number > 0:
-                b = random.randint(1, 12)
-                total = b + modifier
-                totals.append(total)
-                rolls.append(b)
-                modplus.append(k)
-                number = number-1
-            m = list(zip(roll_name, rolls,  modplus, totals))
-            t = tabulate(m, headers=["Roll #", "Results", "Modifier",
-                                     "Totals"
-                                     ]
-                         )
-            await self.bot.say("```" + str(t) + "```")
-        else:
-            await self.bot.say("```" + "Can only roll 20 dice at a time" +
-                               "```")
+        if times < 1:
+            raise RollError("You need to have at least one roll instance.")
+        elif times > 20:
+            raise RollError("Cannot roll more than 20 instances at a time.")
 
-    @dicetable.command(name="d10", pass_context=False)
-    async def _d10_dicetable(self, number: int, modifier: int):
-        """Rolls a number of d10's plus your modifier
-        Example: 'dicetable d10 6 2' rolls 6 d10s with a +2 modifier
-        """
-        if number < 20:
-            roll_name = ["Roll 1", "Roll 2", "Roll 3", "Roll 4", "Roll 5",
-                         "Roll 6", "Roll 7", "Roll 8", "Roll 9", "Roll 10",
-                         "Roll 11",  "Roll 12",  "Roll 13",  "Roll 14",
-                         "Roll 15", "Roll 16",  "Roll 17",  "Roll 18",
-                         "Roll 19",  "Roll 20"
-                         ]
-            rolls = []
-            modplus = []
-            totals = []
+        die, maximum = self.parse_dice(dice)
+        rolls_raw = list(range(times))
+        rolls = [(str("Roll {}".format(x + 1)), self.roll_dice(die, maximum), "+" + str(modifier))
+                 for x in rolls_raw]
+        print(rolls)
+        final = [x + (str(x[1] + modifier),) for x in rolls]
+        headers = ["Roll", "Result", "Modifier", "Total"]
+        t = tabulate(final, headers=headers)
+        await self.bot.say("```{}```".format(t))
 
-            k = "+" + str(modifier)
-            while number > 0:
-                b = random.randint(1, 10)
-                total = b + modifier
-                totals.append(total)
-                rolls.append(b)
-                modplus.append(k)
-                number = number-1
-            m = list(zip(roll_name, rolls,  modplus, totals))
-            t = tabulate(m, headers=["Roll #", "Results", "Modifier",
-                                     "Totals"
-                                     ]
-                         )
-            await self.bot.say("```" + str(t) + "```")
-        else:
-            await self.bot.say("```" + "Can only roll 20 dice at a time" +
-                               "```")
+    def parse_dice(self, dice):
+        parts = re.findall("([0-9]+|[A-Z])", dice, re.I)
+        try:
+            if parts[0].isdigit() and parts[2].isdigit() and parts[1] == "d":
+                die = parts[0]
+                maximum = parts[2]
+                return int(die), int(maximum)
+            else:
+                raise FormatError("Must be in the format of 2d12.")
+        except IndexError:
+            raise FormatError("Must be in the format of 2d12.")
 
-    @dicetable.command(name="d8", pass_context=False)
-    async def _d8_dicetable(self, number: int, modifier: int):
-        """Rolls a number of d8's plus your modifier
-        Example: `dicetable d8 6 2` rolls 6 d8s with a +2 modifier"""
-        if number < 20:
-            roll_name = ["Roll 1", "Roll 2", "Roll 3", "Roll 4", "Roll 5",
-                         "Roll 6", "Roll 7", "Roll 8", "Roll 9", "Roll 10",
-                         "Roll 11",  "Roll 12",  "Roll 13",  "Roll 14",
-                         "Roll 15", "Roll 16",  "Roll 17",  "Roll 18",
-                         "Roll 19",  "Roll 20"
-                         ]
-            rolls = []
-            modplus = []
-            totals = []
-
-            k = "+" + str(modifier)
-            while number > 0:
-                b = random.randint(1, 8)
-                total = b + modifier
-                totals.append(total)
-                rolls.append(b)
-                modplus.append(k)
-                number = number-1
-            m = list(zip(roll_name, rolls,  modplus, totals))
-            t = tabulate(m, headers=["Roll #", "Results", "Modifier",
-                                     "Totals"
-                                     ]
-                         )
-            await self.bot.say("```" + str(t) + "```")
-        else:
-            await self.bot.say("```" + "Can only roll 20 dice at a time" +
-                               "```")
-
-    @dicetable.command(name="d6", pass_context=False)
-    async def _d6_dicetable(self, number: int, modifier: int):
-        """Rolls a number of d6's plus your modifier
-        Example: `dicetable d6 4 2` rolls 4 d6s with a +2 modifier"""
-        if number < 20:
-            roll_name = ["Roll 1", "Roll 2", "Roll 3", "Roll 4", "Roll 5",
-                         "Roll 6", "Roll 7", "Roll 8", "Roll 9", "Roll 10",
-                         "Roll 11",  "Roll 12",  "Roll 13",  "Roll 14",
-                         "Roll 15", "Roll 16",  "Roll 17",  "Roll 18",
-                         "Roll 19",  "Roll 20"
-                         ]
-            rolls = []
-            modplus = []
-            totals = []
-
-            k = "+" + str(modifier)
-            while number > 0:
-                b = random.randint(1, 6)
-                total = b + modifier
-                totals.append(total)
-                rolls.append(b)
-                modplus.append(k)
-                number = number-1
-            m = list(zip(roll_name, rolls,  modplus, totals))
-            t = tabulate(m, headers=["Roll #", "Results", "Modifier",
-                                     "Totals"
-                                     ]
-                         )
-            await self.bot.say("```" + str(t) + "```")
-        else:
-            await self.bot.say("```" + "Can only roll 20 dice at a time" +
-                               "```")
-
-    @dicetable.command(name="d4", pass_context=False)
-    async def _d4_dicetable(self, number: int, modifier: int):
-        """Rolls a number of d4's plus your modifier
-        Example: `dicetable d4 6 2` rolls 6 d4s with a +2 modifier"""
-        if number < 20:
-            roll_name = ["Roll 1", "Roll 2", "Roll 3", "Roll 4", "Roll 5",
-                         "Roll 6", "Roll 7", "Roll 8", "Roll 9", "Roll 10",
-                         "Roll 11",  "Roll 12",  "Roll 13",  "Roll 14",
-                         "Roll 15", "Roll 16",  "Roll 17",  "Roll 18",
-                         "Roll 19",  "Roll 20"
-                         ]
-            rolls = []
-            modplus = []
-            totals = []
-
-            k = "+" + str(modifier)
-            while number > 0:
-                b = random.randint(1, 4)
-                total = b + modifier
-                totals.append(total)
-                rolls.append(b)
-                modplus.append(k)
-                number = number-1
-            m = list(zip(roll_name, rolls,  modplus, totals))
-            t = tabulate(m, headers=["Roll #", "Results", "Modifier",
-                                     "Totals"
-                                     ]
-                         )
-            await self.bot.say("```" + str(t) + "```")
-        else:
-            await self.bot.say("```" + "Can only roll 20 dice at a time" +
-                               "```")
+    def roll_dice(self, die, maximum):
+        roll = 0
+        for x in range(die):
+            roll += random.randint(1, maximum)
+        return roll
 
 
 def setup(bot):
-    bot.add_cog(Dicetable(bot))
+    if tabulateAvailable:
+        bot.add_cog(DiceTable(bot))
+    else:
+        raise RuntimeError("You need to run 'pip3 install tabulate'")
