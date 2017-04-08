3cf8ed649fa0592e1717c38137c64fbb2b2e9937
Redjumpman <Redjumpman@users.noreply.github.com>
2017-04-08 11:52:04 -0500

    Russian Roulette Hotfix 2.1
    Fixed a bug that allowed players to join the circle after the roulette began.

--- russianroulette/russianroulette.py
+++ russianroulette/russianroulette.py
@@ -10,6 +10,47 @@ from .utils import checks
 from __main__ import send_cmd_help
 
 
+kill_message = ["I was really pulling for {0} too. Oh well!",
+                "I guess {0} really wasn't a pea-brain!",
+                "Ahhh now that {0} is gone we can quit playing! No? Ok fine!",
+                ("All things considered, I think we can all agree that {0} was a "
+                 "straight shooter."),
+                "Noooooooo. Not {0}!", "I call dibs on {0}\'s stuff. Too soon?",
+                "Well I guess {0} and I won't be doing that thing anymore...",
+                "Here lies {0}. A loser.", "RIP {0}.", "I kinda hated {0} anyway.",
+                "Hey {0}! I'm back with your snacks! Oh...",
+                "{0}, you\'re like modern art now!", "Called it!",
+                "Really guys? {0}\'s dead? Well this server officially blows now.",
+                "Does this mean I don't have to return the book {0} lent me?",
+                "Oh come on! Now {0}\'s blood is all over my server!",
+                "I\'ll always remember {0}...", "Well at least {0} stopped crying.",
+                "Don\'t look at me. You guys are cleaning up {0}.",
+                "What I'm not crying. *sniff*", "I TOLD YOU, YOU COULD DO IT!",
+                "Well I'm sure someone will miss you, {0}.", "Never forget. {0}."
+                "Yeah. Real smart guys. Just kill off all the fun people.",
+                "I think I got some splatter on me. Gross",
+                "I told you it would blow your mind!", "Well this is fun...",
+                "I go to get popcorn and you all start without me. Rude.",
+                "Oh God. Just before {0} pulled the trigger they shit their pants.",
+                "I guess I\'ll dig this hole a little bigger...",
+                "10/10 would watch {0} blow their brains out again.",
+                "Well I hope {0} has life insurance...",
+                "See you in the next life, {0}", "AND THEIR OFF! Oh... wrong game."
+                "I don\'t know how, but I think {1} cheated.",
+                "{0} always said they wanted to go out with a bang.",
+                "So don\'t sing *another one bites the dust* ?",
+                "I can\'t tell if the smile on {1}\'s face is relief or insanity.",
+                "Oh stop crying {1}. {0} knew what they were getting into.",
+                "So that\'s what a human looks like on the inside!",
+                "My condolences {1}. I know you were *so* close to {0}.",
+                "GOD NO. PLEASE NO. PLEASE GOD NO. NOOOOOOOOOOOOOOOOOOOOOOO!",
+                "Time of death {2}. Cause: Stupidity.", "BOOM HEADSHOT! Sorry..."
+                "Don\'t act like you didn\'t enjoy that, {1}!",
+                "Is it weird that I wish {1} was dead instead?",
+                "Oh real great. {0} dies and I\'m still stuck with {1}. Real. Great.",
+                "Are you eating cheetos? Have some respect {1}! {0} just died!"]
+
+
 class Russianroulette:
     """Allows 6 players to play Russian Roulette"""
 
@@ -17,46 +58,7 @@ class Russianroulette:
         self.bot = bot
         self.file_path = "data/JumperCogs/roulette/russian.json"
         self.system = dataIO.load_json(self.file_path)
-        self.kill_message = ["I was really pulling for {0} too. Oh well!",
-                             "I guess {0} really wasn't a pea-brain!",
-                             "Ahhh now that {0} is gone we can quit playing! No? Ok fine!",
-                             ("All things considered, I think we can all agree that {0} was a "
-                              "straight shooter."),
-                             "Noooooooo. Not {0}!", "I call dibs on {0}\'s stuff. Too soon?",
-                             "Well I guess {0} and I won't be doing that thing anymore...",
-                             "Here lies {0}. A loser.", "RIP {0}.", "I kinda hated {0} anyway.",
-                             "Hey {0}! I'm back with your snacks! Oh...",
-                             "{0}, you\'re like modern art now!", "Called it!",
-                             "Really guys? {0}\'s dead? Well this server officially blows now.",
-                             "Does this mean I don't have to return the book {0} lent me?",
-                             "Oh come on! Now {0}\'s blood is all over my server!",
-                             "I\'ll always remember {0}...", "Well at least {0} stopped crying.",
-                             "Don\'t look at me. You guys are cleaning up {0}.",
-                             "What I'm not crying. *sniff*", "I TOLD YOU, YOU COULD DO IT!",
-                             "Well I'm sure someone will miss you, {0}.", "Never forget. {0}."
-                             "Yeah. Real smart guys. Just kill off all the fun people.",
-                             "I think I got some splatter on me. Gross",
-                             "I told you it would blow your mind!", "Well this is fun...",
-                             "I go to get popcorn and you all start without me. Rude.",
-                             "Oh God. Just before {0} pulled the trigger they shit their pants.",
-                             "I guess I\'ll dig this hole a little bigger...",
-                             "10/10 would watch {0} blow their brains out again.",
-                             "Well I hope {0} has life insurance...",
-                             "See you in the next life, {0}", "AND THEIR OFF! Oh... wrong game."
-                             "I don\'t know how, but I think {1} cheated.",
-                             "{0} always said they wanted to go out with a bang.",
-                             "So don\'t sing *another one bites the dust* ?",
-                             "I can\'t tell if the smile on {1}\'s face is relief or insanity.",
-                             "Oh stop crying {1}. {0} knew what they were getting into.",
-                             "So that\'s what a human looks like on the inside!",
-                             "My condolences {1}. I know you were *so* close to {0}.",
-                             "GOD NO. PLEASE NO. PLEASE GOD NO. NOOOOOOOOOOOOOOOOOOOOOOO!",
-                             "Time of death {2}. Cause: Stupidity.", "BOOM HEADSHOT! Sorry..."
-                             "Don\'t act like you didn\'t enjoy that, {1}!",
-                             "Is it weird that I wish {1} was dead instead?",
-                             "Oh real great. {0} dies and I\'m still stuck with {1}. Real. Great.",
-                             "Are you eating cheetos? Have some respect {1}! {0} just died!"]
-        self.version = "2.1.1"
+        self.version = "2.2"
 
     @commands.group(pass_context=True, no_pm=True)
     async def setrussian(self, ctx):
@@ -133,11 +135,12 @@ class Russianroulette:
                     bank.deposit_credits(mobj, initial_bet)
                     self.reset_game(settings)
                 else:
+                    settings["System"]["Active"] = not settings["System"]["Active"]
                     await self.bot.say("Gather around! The game of russian roulette is starting.\n"
-                                       "I'm going to load a round into this six shot revovler, "
-                                       "give it a good spin, and pass it off to someone at random."
-                                       "If everyone is lucky enough to have a turn, I\'ll start "
-                                       "all over. Good luck!")
+                                       "I'm going to load a round into this six shot **revovler**, "
+                                       "give it a good spin, and pass it off to someone at random. "
+                                       "**If** everyone is lucky enough to have a turn, I\'ll "
+                                       "start all over. Good luck!")
                     await asyncio.sleep(5)
                     await self.roulette_game(settings, server)
                     self.reset_game(settings)
@@ -198,7 +201,7 @@ class Russianroulette:
                 await asyncio.sleep(4)
                 msg = "**BOOM**\n```{} died and was removed from the group.```".format(player.name)
                 await self.bot.say(msg)
-                msg2 = random.choice(self.kill_message)
+                msg2 = random.choice(kill_message)
                 settings["Players"].pop(player.id)
                 remaining = [server.get_member(x) for x in list(settings["Players"].keys())]
                 player2 = random.choice(remaining)
