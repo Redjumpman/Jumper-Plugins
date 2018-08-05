916f59325b03740ec415695a21fe785634057d40
Redjumpman <Redjumpman@users.noreply.github.com>
2018-08-04 19:41:31 -0500

    Update __init__.py

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,5 +1,8 @@
+from redbot.core import data_manager
 from .shop import Shop
 
 
 def setup(bot):
-    bot.add_cog(Shop())
+    cog = Shop()
+    data_manager.load_bundled_data(cog, __file__)
+    bot.add_cog(cog)
