40f112f53c9125a47ff10f11aca3fdb92017ab60
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-02-23 02:01:09 +0100

    it's gonna be a minor bump after all

--- shop/shop.py
+++ shop/shop.py
@@ -31,7 +31,7 @@ __author__ = "Redjumpman"
 
 BaseCog = getattr(commands, "Cog", object)
 
-if version_info < VersionInfo.from_str("3.3.2"):
+if version_info < VersionInfo.from_str("3.4.0"):
     SANITIZE_ROLES_KWARG = {}
 else:
     SANITIZE_ROLES_KWARG = {"sanitize_roles": False}
