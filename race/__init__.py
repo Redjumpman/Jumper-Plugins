7f8516b04d113497cb11a9e65ff64a437abd5917
Dav <dav@mail.stopdavabuse.de>
2020-08-18 16:46:41 +0000

    oops

--- race/__init__.py
+++ race/__init__.py
@@ -1,6 +1,6 @@
 from .race import Race
 
-__end_user_data_statement__:"This cog stores your statistics for wins and losses in the racing game.\nYou may request data deletion through the mydata command."
+__end_user_data_statement__="This cog stores your statistics for wins and losses in the racing game.\nYou may request data deletion through the mydata command."
 
 def setup(bot):
     bot.add_cog(Race())
