1d1d49d8df4f98c7ed844cb12712539ea61b46f0
aikaterna <20862007+aikaterna@users.noreply.github.com>
2020-08-26 09:03:43 -0700

    phrasing

--- casino/__init__.py
+++ casino/__init__.py
@@ -1,6 +1,6 @@
 from .casino import Casino
 
-__red_end_user_data_statement__ = "This cog does store discord IDs as needed for operation."
+__red_end_user_data_statement__ = "This cog stores discord IDs as needed for operation."
 
 
 async def setup(bot):
