5e6f1a281ff81bc7147ee6b38163eb080bc6e202
aikaterna <20862007+aikaterna@users.noreply.github.com>
2021-01-14 22:03:40 -0800

    [Coupon] Small fixes

--- coupon/coupon.py
+++ coupon/coupon.py
@@ -1,7 +1,9 @@
+import discord
 import uuid
 
 from redbot.core import bank, checks, commands, Config
-from redbot.core.utils.chat_formatting import box, pagify
+from redbot.core.errors import BalanceTooHigh
+from redbot.core.utils.chat_formatting import box, humanize_number, pagify
 
 
 async def pred(ctx):
@@ -52,14 +54,19 @@ class Coupon(commands.Cog):
     @global_bank_check
     async def _create_coupon(self, ctx, credits: int):
         """Generates a unique coupon code."""
+        if credits > 2 ** 63 - 1:
+            return await ctx.send("Try a smaller number.")
+        if credits < 0:
+            return await ctx.send("Nice try.")
         code = str(uuid.uuid4())
         try:
             settings = await self.config.guild(ctx.guild).coupons()
-            settings.update({coupon: credits})
+            settings.update({code: credits})
             await self.config.guild(ctx.guild).coupons.set(settings)
-            await ctx.author.send(f"I have created a coupon for `{credits}` credits.\nThe code is: `{code}`")
+            credits_name = await bank.get_currency_name(ctx.guild)
+            await ctx.author.send(f"I have created a coupon for `{humanize_number(credits)}` {credits_name}.\nThe code is: `{code}`")
         except discord.Forbidden:
-            await ctx.author.send("I was not able to DM you the code. Please open your DMs and try again.")
+            await ctx.send("I was not able to DM you the code. Please open your DMs and try again.")
 
     @coupon.command(name="list")
     @checks.admin_or_permissions(manage_guild=True)
@@ -73,22 +80,28 @@ class Coupon(commands.Cog):
             msg += "No active codes."
         else:
             for code, credits in settings.items():
-                msg += f"{code} | {credits}\n"
+                msg += f"{code} | {humanize_number(credits)}\n"
         for text in pagify(msg):
             await ctx.send(box(text, lang="ini"))
 
     @coupon.command(name="redeem")
     @global_bank_check
     async def _redeem_coupon(self, ctx, coupon: str):
-        """Redeems a coupon code, can be done through DM with the bot."""
+        """Redeems a coupon code."""
         if len(coupon) == 36:
             settings = await self.config.guild(ctx.guild).coupons()
             if coupon in settings:
                 credits = settings[coupon]
-                await bank.deposit_credits(ctx.author, credits)
+                credits_name = await bank.get_currency_name(ctx.guild)
+                try:
+                    await bank.deposit_credits(ctx.author, credits)
+                    extra = ""
+                except BalanceTooHigh as e:
+                    await bank.set_balance(ctx.author, e.max_balance)
+                    extra = f"You've hit the economy cap of {humanize_number(e.max_balance)} {credits_name}, though."
                 del settings[coupon]
                 await self.config.guild(ctx.guild).coupons.set(settings)
-                await ctx.send(f"I have added {credits} credits to your account.")
+                await ctx.send(f"I have added {humanize_number(credits)} {credits_name} to your account. {extra}")
             else:
                 await ctx.send("This coupon either does not exist or has already been redeemed.")
         else:
