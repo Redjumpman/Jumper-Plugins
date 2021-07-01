36d21f21719e7039a1429eb998bc3e1b6cb265a4
Brad Duncan <51077147+XargsUK@users.noreply.github.com>
2021-07-01 01:38:32 +0100

    Update games.py

--- casino/games.py
+++ casino/games.py
@@ -294,7 +294,7 @@ class Blackjack:
             count = deck.bj_count(dh)
 
         # defines maximum hit score X
-        while count < 17:
+        while count < 22:
             deck.deal(hand=dh)
             count = deck.bj_count(dh)
         return dh
