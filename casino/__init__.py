7902f5e61aca41465570e56492458c793faf6795
Alex <58824393+vertyco@users.noreply.github.com>
2022-08-18 18:42:57 -0400

    cross dpy compatible

--- casino/__init__.py
+++ casino/__init__.py
@@ -1,9 +1,13 @@
 from .casino import Casino
+import discord
 
 __red_end_user_data_statement__ = "This cog stores discord IDs as needed for operation."
 
 
 async def setup(bot):
     cog = Casino(bot)
-    bot.add_cog(cog)
+    if discord.__version__ > "1.7.3":
+        await bot.add_cog(cog)
+    else:
+        bot.add_cog(cog)
     await cog.initialise()
