e0bf938fd0603c1785e1bff51b928debf38f947a
Leet <36166244+leetfin@users.noreply.github.com>
2022-12-29 22:30:05 -0500

    [shop] fix loading on dpy2

--- shop/__init__.py
+++ shop/__init__.py
@@ -3,6 +3,6 @@ from .shop import Shop
 __red_end_user_data_statement__ = "This cog stores discord IDs as needed for operation."
 
 
-def setup(bot):
+async def setup(bot):
     cog = Shop()
-    bot.add_cog(cog)
+    await bot.add_cog(cog)
